b131414da4885ba06d86629ac9629118093901df
abp437 <abp437@gmail.com>
2017-10-02 17:26:47 +0530

    Basic Routing Implemented

--- Node JS/server.js
+++ Node JS/server.js
@@ -1,17 +1,4 @@
 var http = require('http');
-var fs = require('fs');
+var router = require('./router');
 
-function onRequest(request, response) {
-	console.log(request.headers);
-	response.writeHead(200, {'Content-Type': 'text/html'});
-	fs.readFile('./index.html', null, function(error, data) {
-		if(error) {
-			response.writeHead(404, 'Error Encountered', {'Content-Type': 'text/html'});
-		} else {
-			response.write(data);
-		}
-		response.end();
-	});
-}
-
-http.createServer(onRequest).listen(8000);
+http.createServer(router.handleRequest).listen(8000);
