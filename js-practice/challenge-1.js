9f09aa0f9f311093477fce0e84a8cb583c43f7e0
abp437 <abp437@gmail.com>
2018-09-08 12:18:51 +0530

    Challenge 1 Done

--- js-practice/challenge-1.js
+++ js-practice/challenge-1.js
@@ -9,12 +9,26 @@ Use ES6 whenever possible.
 "use strict";
 
 // Write code here
+const sum = (...restArgs) => restArgs.reduce((total, num) => total + num);
 
-sum(1, 3);
+// Explanation:
+// Here I have used Rest Parameter for taking variable number of arguments.
+// In the code snippet …restArgs creates an array restArgs that holds all the
+// arguments passed to the function in an array. So basically it's like passing
+// an array as an argument to a function. Since restArgs is an array I can apply
+// array methods on it.
+// Also here I've used reduce since we need to get a single value from the
+// function which is related to the result of the previous iteration of the
+// operation.
+// So reduce takes an array then performs some operation on it and then returns
+// a value as an argument to the next iteration until the last value is
+// evaluated and returned.
+
+console.log(sum(1, 3));
 //4
 
-sum(10, 20, 5);
+console.log(sum(10, 20, 5));
 //35
 
-sum(2, 5, 80, 1, 10, 12);
+console.log(sum(2, 5, 80, 1, 10, 12));
 //110
