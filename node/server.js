6b73d68a3ba968778195d56043ea20c13c5f1e68
abp437 <abp437@gmail.com>
2019-01-01 02:27:05 +0530

    Dependencies added and Linting Rules added

--- node/server.js
+++ node/server.js
@@ -1,44 +1,59 @@
 const express = require('express'),
-  MongoClient = require('mongodb').MongoClient,
+  {
+    MongoClient,
+  } = require('mongodb'),
   assert = require('assert'),
   dbUrl = 'mongodb://localhost:27017',
   dbName = 'users',
   dbClient = new MongoClient(dbUrl),
   port = process.env.PORT || 3000,
-  app = express();
+  app = express(),
 
-
-app.get('/', (req, res) => res.send('Hello World'));
-
-const insertUser = (db, callback) => {
-  const docs = db.collection('users').insertOne({
-    name: 'Akshay'
-  }, (err, response) => {
-    console.log(response.ops);
-  });
-  callback(docs);
-};
-
-const retrieveUser = (db, callback) => {
-  db.collection('users').find({
-    name: 'Akshay'
-  }).toArray((err, docs) => {
-    assert.equal(err, null);
-    console.log("Found the Following Records");
-    console.log(docs);
+  // Create User
+  insertUser = (db, callback) => {
+    const docs = db.collection('users').insertOne({
+      name: 'Akshay',
+    }, (err, response) => {
+      console.log(response.ops);
+    });
     callback(docs);
-  });
-};
+  },
 
-dbClient.connect(err => {
+  // Read User
+  retrieveUser = (db, callback) => {
+    db.collection('users').find({
+      name: 'Akshay',
+    }).toArray((err, docs) => {
+      assert.equal(err, null);
+      console.log('Found the Following Records');
+      console.log(docs);
+      callback(docs);
+    });
+  },
+
+  // Delete User
+  deleteUser = (db) => {
+    db.collection('users').deleteMany({
+      name: 'Akshay',
+    }, (err) => {
+      if (err) throw err;
+      console.log('Deleted Successfully');
+    });
+  };
+
+// DB Connection
+dbClient.connect((err) => {
   const db = dbClient.db(dbName);
   assert.equal(null, err);
-  console.log("Connected To Database Server");
-  insertUser(db, () => {
-    retrieveUser(db, () => {
-      dbClient.close();
-    });
-  });
+  console.log('Connected To Database Server');
+  insertUser(db, () => {});
+  retrieveUser(db, () => {});
+  deleteUser(db, () => {});
+  dbClient.close();
 });
 
+// Routes For REST API
+app.get('/', (req, res) => res.send('Hello World'));
+
+// Server Start
 app.listen(port, () => console.log(`Listening on Port ${port}`));
