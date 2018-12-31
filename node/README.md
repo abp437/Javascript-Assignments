664be15b4f75036d5f8684a6ff207727bd55d14b
abp437 <abp437@gmail.com>
2018-12-31 22:37:08 +0530

    Basic Create and Get method implemented

---
+++ node/README.md
@@ -0,0 +1,10 @@
+This document is a Basic RESTful API service in NodeJS(Express).
+
+The Server runs on 3000.
+
+Starting off DB:
+cd "C:\Program Files\MongoDB\Server\3.6\bin\"
+./mongod --dbpath="C:\data\db"
+
+Starting Off Server:
+nodemon server
--- node/server.js
+++ node/server.js
@@ -1,7 +1,44 @@
 const express = require('express'),
-  port = 3000,
+  MongoClient = require('mongodb').MongoClient,
+  assert = require('assert'),
+  dbUrl = 'mongodb://localhost:27017',
+  dbName = 'users',
+  dbClient = new MongoClient(dbUrl),
+  port = process.env.PORT || 3000,
   app = express();
 
+
 app.get('/', (req, res) => res.send('Hello World'));
+
+const insertUser = (db, callback) => {
+  const docs = db.collection('users').insertOne({
+    name: 'Akshay'
+  }, (err, response) => {
+    console.log(response.ops);
+  });
+  callback(docs);
+};
+
+const retrieveUser = (db, callback) => {
+  db.collection('users').find({
+    name: 'Akshay'
+  }).toArray((err, docs) => {
+    assert.equal(err, null);
+    console.log("Found the Following Records");
+    console.log(docs);
+    callback(docs);
+  });
+};
+
+dbClient.connect(err => {
+  const db = dbClient.db(dbName);
+  assert.equal(null, err);
+  console.log("Connected To Database Server");
+  insertUser(db, () => {
+    retrieveUser(db, () => {
+      dbClient.close();
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Listening on Port ${port}`));
