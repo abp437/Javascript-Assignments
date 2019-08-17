e359c76a65b495f11d3c8700288a33f285802c14
abp437 <abp437@gmail.com>
2019-08-17 17:17:12 +0530

    Context Provider and Consumer added

--- react-hooks/src/components/Booklist.js
+++ react-hooks/src/components/Booklist.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
   render() {
     return (
-      <div>
-        <ul className="book-list">
-          <li>The way of kings</li>
-          <li>The name of the wind</li>
-          <li>The final empire</li>
-        </ul>
-      </div>
-     );
+      <ThemeContext.Consumer>{(context) => {
+        const { isLightTheme, light, dark, } = context;
+        const theme = isLightTheme ? light : dark;
+        const { bg, syntax } = theme;
+        return (
+          <div className="book-list" style={{ color: syntax, background: bg }}>
+            <ul>
+              <li style={{ background: bg }}>The way of kings</li>
+              <li style={{ background: bg }}>The name of the wind</li>
+              <li style={{ background: bg }}>The final empire</li>
+            </ul>
+          </div>
+        );
+      }}
+      </ThemeContext.Consumer>
+    );
   }
 }
 
--- react-hooks/src/components/Navbar.js
+++ react-hooks/src/components/Navbar.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
   render() {
     return (
-      <React.Fragment>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </React.Fragment>
-     );
+      <ThemeContext.Consumer>{(context) => {
+        const { isLightTheme, light, dark, } = context;
+        const theme = isLightTheme ? light : dark;
+        const { ui, syntax } = theme;
+        return (
+          <nav style={{ background: ui, color: syntax }}>
+            <h1>Context App</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        );
+      }}
+      </ThemeContext.Consumer>
+    );
   }
 }
 
